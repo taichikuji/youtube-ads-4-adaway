7752ffcb5a47c930c05cb72b8b0febb44ab9318f
Iván Pérez <48720361+taichikuji@users.noreply.github.com>
2021-02-09 11:12:03 +0100

    Update script
    Seems like I knew how uniq worked. I was wrong.
    
    This should fix it.
    
    This also includes a squashed automatic commit

--- adaway_script.sh
+++ adaway_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-links=("https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/black.list" "https://raw.githubusercontent.com/Ewpratten/youtube_ad_blocklist/master/blocklist.txt")
+links=("https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/black.list" "https://raw.githubusercontent.com/Ewpratten/youtube_ad_blocklist/master/blocklist.txt" "https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/youtubelist.txt")
 for link in "${links[@]}"
 do
     curl $link --output black.list
@@ -7,5 +7,5 @@ do
     rm black.list
 done
 
-sort list | uniq -u > hosts
+sort list | uniq > hosts
 rm list

4c250621bf295406d780004c2b33b03bff164ac2
Taichi <ivanperezfernandezsuper@gmail.com>
2021-11-17 00:18:54 +0000

    Edit scripts and workflow

--- adaway_script.ps1
+++ adaway_script.ps1
@@ -1,19 +1,17 @@
 $links = @("https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/black.list", "https://raw.githubusercontent.com/Ewpratten/youtube_ad_blocklist/master/blocklist.txt")
-function append-text { 
+function Append-text { 
     process{
         foreach-object {"0.0.0.0 " + $_}
      }
     }
 
-$time = Get-Date
 ForEach ($link in $links){
     $list = "$PSScriptRoot\" + $links.IndexOf($link) + ".lists"
     (New-Object System.Net.WebClient).DownloadFile($link, $list)
     If (Test-Path -Path $list) {
-	    Get-content $list | append-text | Add-Content -Path "$PSScriptRoot\list"
+	    Get-content $list | Append-text | Add-Content -Path "$PSScriptRoot\list"
     }
     Remove-Item -Path $list -Force
 }
 Get-content "$PSScriptRoot\list" | Sort-Object -Unique | Set-Content "$PSScriptRoot\hosts"
 Remove-Item -Path "$PSScriptRoot\list" -Force
-Write-output "Time taken to make your list available for Adaway: $((Get-Date).Subtract($time).Seconds) seconds(s)"

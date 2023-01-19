8166c9b0e09fe5d875ce13bcecbaa70d9a81598b
Iván Pérez <48720361+taichikuji@users.noreply.github.com>
2023-01-19 09:22:52 +0000

    Add additional source to scripts

--- adaway_script.sh
+++ adaway_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-links=("https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/black.list" "https://raw.githubusercontent.com/Ewpratten/youtube_ad_blocklist/master/blocklist.txt")
+links=("https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/black.list" "https://raw.githubusercontent.com/kliff-k/YtAdList/main/youtube.txt" "https://raw.githubusercontent.com/Ewpratten/youtube_ad_blocklist/master/blocklist.txt")
 for link in "${links[@]}"
 do
-    curl $link --output black.list
+    curl --silent --output black.list $link
     sed 's/^/0.0.0.0 /' black.list >> list
     rm black.list
 done

ec9d586fd55a69e07fdc8c570297b9a122ce2a48
Iván Pérez <48720361+taichikuji@users.noreply.github.com>
2023-06-02 10:47:54 +0000

    Optimized ps1 and sh scripts

--- adaway_script.ps1
+++ adaway_script.ps1
@@ -1,17 +1,15 @@
-$links = @("https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/black.list", "https://raw.githubusercontent.com/Ewpratten/youtube_ad_blocklist/master/blocklist.txt", "https://raw.githubusercontent.com/kliff-k/YtAdList/main/youtube.txt")
-function Append-text { 
-    process{
-        foreach-object {"0.0.0.0 " + $_}
-     }
-    }
+$links = @("https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/black.list","https://raw.githubusercontent.com/Ewpratten/youtube_ad_blocklist/master/blocklist.txt","https://raw.githubusercontent.com/kliff-k/YtAdList/main/youtube.txt")
 
-ForEach ($link in $links){
-    $list = "$PSScriptRoot\" + $links.IndexOf($link) + ".lists"
+foreach ($link in $links) {
+    $list = Join-Path $PSScriptRoot ($links.IndexOf($link) + ".lists")
     (New-Object System.Net.WebClient).DownloadFile($link, $list)
-    If (Test-Path -Path $list) {
-	    Get-content $list | Append-text | Add-Content -Path "$PSScriptRoot\list"
+
+    if (Test-Path -Path $list) {
+        Get-Content $list | ForEach-Object { "0.0.0.0 $_" }
+        Remove-Item -Path $list -Force
     }
-    Remove-Item -Path $list -Force
 }
-Get-content "$PSScriptRoot\list" | Sort-Object -Unique | Set-Content "$PSScriptRoot\hosts"
-Remove-Item -Path "$PSScriptRoot\list" -Force
+
+Get-Content "$PSScriptRoot\*.lists" | Sort-Object -Unique | Set-Content (Join-Path $PSScriptRoot "hosts")
+
+Remove-Item -Path "$PSScriptRoot\*.lists" -Force
--- adaway_script.sh
+++ adaway_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 links=("https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/black.list" "https://raw.githubusercontent.com/kliff-k/YtAdList/main/youtube.txt" "https://raw.githubusercontent.com/Ewpratten/youtube_ad_blocklist/master/blocklist.txt")
-for link in "${links[@]}"
-do
-    curl --silent --output black.list $link
-    sed 's/^/0.0.0.0 /' black.list >> list
-    rm black.list
+
+for link in "${links[@]}"; do
+    curl -s "$link" -o "black.list"
+    sed 's/^/0.0.0.0 /' "black.list" >> "list"
+    rm "black.list"
 done
 
-sort list | uniq > hosts
-rm list
+sort "list" | uniq > "hosts"
+rm "list"

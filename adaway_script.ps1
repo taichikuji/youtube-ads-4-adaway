ca325388fe70ae7f4711c17a42706851dde9e955
Iván Pérez <48720361+taichikuji@users.noreply.github.com>
2023-06-14 19:16:42 +0000

    Optimize adaway_script.ps1

--- adaway_script.ps1
+++ adaway_script.ps1
@@ -1,17 +1,17 @@
 $links = @("https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/black.list", "https://raw.githubusercontent.com/Ewpratten/youtube_ad_blocklist/master/blocklist.txt", "https://raw.githubusercontent.com/kliff-k/YtAdList/main/youtube.txt")
-function Append-text { 
-    process{
-        foreach-object {"0.0.0.0 " + $_}
-     }
-    }
 
-ForEach ($link in $links){
-    $list = "$PSScriptRoot\" + $links.IndexOf($link) + ".lists"
-    (New-Object System.Net.WebClient).DownloadFile($link, $list)
-    If (Test-Path -Path $list) {
-	    Get-content $list | Append-text | Add-Content -Path "$PSScriptRoot\list"
+function Append-Text {
+    process {
+        foreach-object { "0.0.0.0 $_" }
     }
-    Remove-Item -Path $list -Force
 }
-Get-content "$PSScriptRoot\list" | Sort-Object -Unique | Set-Content "$PSScriptRoot\hosts"
-Remove-Item -Path "$PSScriptRoot\list" -Force
+
+$hosts = @()
+
+ForEach ($link in $links) {
+    $content = (New-Object System.Net.WebClient).DownloadString($link)
+    $processedContent = $content.Split([Environment]::NewLine) | Append-Text
+    $hosts += $processedContent
+}
+
+$hosts | Sort-Object -Unique | Set-Content (Join-Path $PSScriptRoot "hosts")

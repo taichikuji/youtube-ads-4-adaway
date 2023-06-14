71d30b19617ac9f4371ee652d076d4730d680664
Iván Pérez <48720361+taichikuji@users.noreply.github.com>
2023-06-14 19:15:43 +0000

    Revert changes on adaway_script.ps1

--- adaway_script.ps1
+++ adaway_script.ps1
@@ -1,15 +1,17 @@
-$links = @("https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/black.list","https://raw.githubusercontent.com/Ewpratten/youtube_ad_blocklist/master/blocklist.txt","https://raw.githubusercontent.com/kliff-k/YtAdList/main/youtube.txt")
-
-foreach ($link in $links) {
-    $list = Join-Path $PSScriptRoot ($links.IndexOf($link) + ".lists")
-    (New-Object System.Net.WebClient).DownloadFile($link, $list)
-
-    if (Test-Path -Path $list) {
-        Get-Content $list | ForEach-Object { "0.0.0.0 $_" }
-        Remove-Item -Path $list -Force
+$links = @("https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/black.list", "https://raw.githubusercontent.com/Ewpratten/youtube_ad_blocklist/master/blocklist.txt", "https://raw.githubusercontent.com/kliff-k/YtAdList/main/youtube.txt")
+function Append-text { 
+    process{
+        foreach-object {"0.0.0.0 " + $_}
+     }
     }
+
+ForEach ($link in $links){
+    $list = "$PSScriptRoot\" + $links.IndexOf($link) + ".lists"
+    (New-Object System.Net.WebClient).DownloadFile($link, $list)
+    If (Test-Path -Path $list) {
+	    Get-content $list | Append-text | Add-Content -Path "$PSScriptRoot\list"
+    }
+    Remove-Item -Path $list -Force
 }
-
-Get-Content "$PSScriptRoot\*.lists" | Sort-Object -Unique | Set-Content (Join-Path $PSScriptRoot "hosts")
-
-Remove-Item -Path "$PSScriptRoot\*.lists" -Force
+Get-content "$PSScriptRoot\list" | Sort-Object -Unique | Set-Content "$PSScriptRoot\hosts"
+Remove-Item -Path "$PSScriptRoot\list" -Force
